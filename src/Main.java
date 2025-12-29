148aca290a4c13097e7f53ebdaaf4062bd213f3e
alexmet2 <103521345+alexmet2@users.noreply.github.com>
2025-12-29 18:05:13 +0200

    Adjacency Lists

--- src/Main.java
+++ src/Main.java
@@ -16,6 +16,8 @@ void main() throws IOException {
     InputStreamReader();
     System.out.println("RandomStreamPrediction");
     RandomStreamPrediction();
+    System.out.println("Adjacency Lists");
+    Adjacency_Lists();
 }
 
 void Map_Collect(){
@@ -68,4 +70,80 @@ void RandomStreamPrediction(){
     Stream<Integer> rand = Stream.generate(()->(new Random()).nextInt(81));
     System.out.println("The numbers you should play in the next Kino game is ");
     rand.limit(5).forEach(System.out::println);
+}
+
+void Adjacency_Lists(){
+    /*
+        Adjacency List = An Array/ArrayAist of LinkedList
+                         Each LinkedList has a unique node at the head
+                         All adjacent neighbors to that node are added to that node's LinkedList
+
+                         runtime complexity to check an Edge: O(v)
+                         space complexity: O(v+e)
+    */
+    /* declares graph */
+    Graph graph = new Graph();
+    /* we add a node in the graph */
+    graph.addNode(new Node('A'));
+    graph.addNode(new Node('B'));
+    graph.addNode(new Node('C'));
+    graph.addNode(new Node('D'));
+    graph.addNode(new Node('E'));
+    /* we add edges to our graph */
+    graph.addEdge(0, 1);
+    graph.addEdge(1, 2);
+    graph.addEdge(1, 4);
+    graph.addEdge(2, 3);
+    graph.addEdge(2, 4);
+    graph.addEdge(4, 0);
+    graph.addEdge(4, 2);
+    /* prints graph */
+    graph.print();
+}
+
+static class Node {
+
+    char data;
+
+    Node(char data){
+        this.data = data;
+    }
+}
+
+static class Graph {
+
+    ArrayList<LinkedList<Node>> alist;
+
+    Graph(){
+        alist = new ArrayList<>();
+    }
+    public void addNode(Node node){
+        LinkedList<Node> currentList = new LinkedList<>();
+        currentList.add(node);
+        alist.add(currentList);
+    }
+    public void addEdge(int src, int dst){
+        LinkedList<Node> currentList = alist.get(src);
+        Node dstNode = alist.get(dst).getFirst();
+        currentList.add(dstNode);
+    }
+    public boolean checkEdge(int src, int dst){
+        LinkedList<Node> currentList = alist.get(src);
+        Node dstNode = alist.get(dst).getFirst();
+
+        for(Node node : currentList){
+            if(node == dstNode){
+                return true;
+            }
+        }
+        return false;
+    }
+    public void print(){
+        for(LinkedList<Node> currentList : alist){
+            for(Node node : currentList){
+                System.out.print(node.data + " -> ");
+            }
+            System.out.println();
+        }
+    }
 }
