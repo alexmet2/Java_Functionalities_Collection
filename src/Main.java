f9b24714c6af37261a0827c33732b145397784c9
alexmet2 <103521345+alexmet2@users.noreply.github.com>
2025-12-29 18:46:21 +0200

    Insertion Sort

--- src/Main.java
+++ src/Main.java
@@ -30,6 +30,8 @@ void main() throws IOException {
     Recursion();
     System.out.println("Selection Sort 1");
     SelectionSort1();
+    System.out.println("InsertionSort 1");
+    InsertionSort1();
 }
 void Map_Collect(){
     List<Integer> l1 = Arrays.asList(1,2,3,4,5);
@@ -595,4 +597,50 @@ static void selectionSortDescending(int[] array) {
         array[i] = array[min];
         array[min] = temp;
     }
+}
+void InsertionSort1(){
+    /*
+        Insertion Sort = after comparing elements to the left
+                         shift elements to the right to make room to insert a value
+
+                         Quadratic time = O(n^2)
+                         small data set = decent
+                         large data set = BAD
+
+                         Fewer steps than Bubble Sort
+                         Best case is O(n) compared to Selection Sort
+    */
+    System.out.println("Without sort: ");
+    int[] array = {9, 1, 8, 2, 7, 3, 6, 5, 4};
+    /* iterates through array */
+    for(int i : array){
+        System.out.print(i + " ");
+    }
+    System.out.println();
+    System.out.println("InsertionSort 2");
+    InsertionSort2();
+}
+static void InsertionSort2(){
+    System.out.println("With sort: ");
+    int[] array = {9, 1, 8, 2, 7, 3, 6, 5, 4};
+    /* calls method InsertionSorts */
+    InsertionSorts(array);
+    /* iterates through array */
+    for(int i : array){
+        System.out.print(i + " ");
+    }
+}
+static void InsertionSorts(int[] array) {
+
+    for(int i = 1; i < array.length; i++){
+        int temp = array[i];
+        int j = i - 1;
+
+        while(j >= 0 && array[j] > temp){
+            array[j + 1] = array[j];
+            j--;
+        }
+        array[j + 1] = temp;
+    }
+
 }
