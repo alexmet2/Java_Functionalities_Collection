90f0aad58b2e40655f6bc2e19521b49fbca2f9e4
alexmet2 <103521345+alexmet2@users.noreply.github.com>
2025-12-29 17:45:17 +0200

    Map_Collect

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,9 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 void main() {
-  //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-  // to see how IntelliJ IDEA suggests fixing it.
-  IO.println(String.format("Hello and welcome!"));
+    Map_Collect();
+}
 
-  for (int i = 1; i <= 5; i++) {
-    //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-    // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-    IO.println("i = " + i);
-  }
+void Map_Collect(){
+    List<Integer> l1 = Arrays.asList(1,2,3,4,5);
+    List<Integer> l2 = l1.stream().map(x->x*x).toList();
+    for(Integer i : l2) System.out.println(i);
 }
