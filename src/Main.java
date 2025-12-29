b7fe857c3b77e3dcf6e424993c627d46b148ec5d
alexmet2 <103521345+alexmet2@users.noreply.github.com>
2025-12-29 17:48:37 +0200

    Reduce

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,25 @@
 void main() {
+    System.out.println("Map Collect");
     Map_Collect();
+    System.out.println("Reduce");
+    Reduce();
 }
 
 void Map_Collect(){
     List<Integer> l1 = Arrays.asList(1,2,3,4,5);
     List<Integer> l2 = l1.stream().map(x->x*x).toList();
     for(Integer i : l2) System.out.println(i);
+}
+
+void Reduce(){
+    int[] ar = {1,2,3,4,5,6,7,8,9,10};
+    int sum = 0;
+    for(int i : ar) sum += i;
+    // OR
+    // Reduce method
+    int sum1 = Arrays.stream(ar).reduce(0,(x,y) -> x+y);
+    int sum2 = Arrays.stream(ar).reduce(0, Integer::sum);
+    System.out.println(sum);
+    System.out.println(sum1);
+    System.out.println(sum2);
 }
