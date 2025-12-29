3e535e1d886bcd4e7622b70c81455b0c420261e2
alexmet2 <103521345+alexmet2@users.noreply.github.com>
2025-12-29 18:36:25 +0200

    Merge Sort

--- src/Main.java
+++ src/Main.java
@@ -24,6 +24,8 @@ void main() throws IOException {
     AdjacencyMatrix();
     System.out.println("HashTable 1");
     HashTable1();
+    System.out.println("Merge Sort 1");
+    MergeSort1();
 }
 void Map_Collect(){
     List<Integer> l1 = Arrays.asList(1,2,3,4,5);
@@ -307,7 +309,7 @@ void HashTable1(){
     System.out.println("HashTable 4");
     HashTable4();
 }
-public static void HashTable2(){
+static void HashTable2(){
     Hashtable<Integer,String> table = new Hashtable<>(10);
     /* adding elements in the Hashtable */
     table.put(100, "SpongeBob");
@@ -323,7 +325,7 @@ public static void HashTable2(){
     }
     System.out.println();
 }
-public static void HashTable3(){
+static void HashTable3(){
     Hashtable<Integer,String> table = new Hashtable<>(10);
     /* adding elements in the Hashtable */
     table.put(100, "SpongeBob");
@@ -346,7 +348,7 @@ public static void HashTable3(){
     }
     System.out.println();
 }
-public static void HashTable4(){
+static void HashTable4(){
     Hashtable<String,String> table = new Hashtable<>(10);
     /* adding elements in the Hashtable */
     table.put("100", "SpongeBob");
@@ -372,5 +374,90 @@ public static void HashTable4(){
     System.out.println("\nOR\n");
     for(String key : table.keySet()){
         System.out.println(key.hashCode() % 10 + "\t"+ key + "\t" + table.get(key));
+    }
+}
+void MergeSort1(){
+    /*
+        Merge Sort = Recursively divide array in 2, sort, re-combine
+                     Run-time complexity = (n log n)
+                     Space complexity = 0(n)
+    */
+    int[] array = {8, 2, 5, 3, 4, 7, 6, 1};
+    System.out.println("Without sort: ");
+    /* iterates through the array */
+    for (int j : array) {
+        System.out.print(j + " ");
+    }
+    System.out.println();
+    System.out.println("Merge Sort 2");
+    MergeSort2();
+}
+static void MergeSort2(){
+    int[] array = {8, 2, 5, 3, 4, 7, 6, 1};
+    System.out.println("With sort: ");
+    /* calls the mergeSort method */
+    mergeSort(array);
+    /* iterates through the array */
+    for (int j : array) {
+        System.out.print(j + " ");
+    }
+}
+static void mergeSort(int[] array){
+
+    int length = array.length;
+    if (length <= 1) return; /* base case */
+
+    int middle = length / 2;
+    int[] leftArray = new int[middle];
+    int[] rightArray = new int[length - middle];
+
+    int i = 0; /* for left array */
+    int j = 0; /* for right array */
+
+    for(; i < length; i++){
+        if(i < middle){
+            leftArray[i] = array[i];
+        }
+        else{
+            rightArray[j] = array[i];
+            j++;
+        }
+    }
+
+    mergeSort(leftArray);
+    mergeSort(rightArray);
+    merge(leftArray, rightArray, array);
+}
+static void merge(int[] leftArray, int[] rightArray, int[] array){
+
+    int leftSize = array.length / 2;
+    int rightSize = array.length - leftSize;
+    /* indices */
+    int i = 0; /* for the original array used to keep track of the position */
+    int l = 0; /* for leftArray */
+    int r = 0; /* for rightArray */
+
+    /* check the conditions for merging */
+    while(l < leftSize && r < rightSize){
+        if (leftArray[l] < rightArray[r]){
+            array[i] = leftArray[l];
+            i++;
+            l++;
+        }
+        else{
+            array[i] = rightArray[r];
+            i++;
+            r++;
+        }
+    }
+    while(l < leftSize){
+        array[i] = leftArray[l];
+        i++;
+        l++;
+    }
+    while(r < rightSize){
+        array[i] = rightArray[r];
+        i++;
+        r++;
     }
 }
